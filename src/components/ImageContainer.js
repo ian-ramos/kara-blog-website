09a72ad1f8f96616bf21388e81b72580e89e27e1
ian-ramos <ramos.ianc@gmail.com>
2018-02-20 14:41:21 -0500

    refactored code to use inline style for background image as opposed to a separate id in CSS

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -1,5 +1,16 @@
 import React from 'react'
 import '../App.css'
+import bambooTumalogFalls from '../images/bamboo-tumalog-falls.jpg'
+import irishHungerMemorial from '../images/irish-hunger-memorial.jpg'
+import lanternFestival from '../images/lantern-festival.jpg'
+import sanRemigioBeach from '../images/san-remigio-beach.jpg'
+import siriYanguBalcony from '../images/siri-yangu-balcony.jpg'
+import siriYanguBoat from '../images/siri-yangu-boat.jpg'
+import siriYanguBridge from '../images/siri-yangu-bridge.jpg'
+import siriYanguPalmTree from '../images/siri-yangu-palm-tree.jpg'
+import siriYanguTree from '../images/siri-yangu-tree.jpg'
+import west35Pic1 from '../images/west35-pic1.jpg'
+import west35Pic2 from '../images/west35-pic2.jpg'
 
 class ImageContainer extends React.Component {
 
@@ -10,27 +21,27 @@ class ImageContainer extends React.Component {
   render() {
     return (
       <div>
-        <section className="portfolio-image-vertical" id="bamboo-tumalog-falls" onMouseEnter={this.clicked}></section>
+        <section className="portfolio-image-vertical" style={{backgroundImage: `url(${bambooTumalogFalls})`}} onMouseEnter={this.clicked}></section>
         <div className="separator"></div>
-        <section className="portfolio-image-horizontal" id="irish-hunger-memorial"></section>
+        <section className="portfolio-image-horizontal" style={{backgroundImage: `url(${irishHungerMemorial})`}}></section>
         <div className="separator"></div>
-        <section className="portfolio-image-horizontal" id="lantern-festival"></section>
+        <section className="portfolio-image-horizontal" style={{backgroundImage: `url(${lanternFestival})`}}></section>
         <div className="separator"></div>
-        <section className="portfolio-image-vertical" id="san-remigio-beach"></section>
+        <section className="portfolio-image-vertical" style={{backgroundImage: `url(${sanRemigioBeach})`}}></section>
         <div className="separator"></div>
-        <section className="portfolio-image-vertical" id="siri-yangu-bridge"></section>
+        <section className="portfolio-image-vertical" style={{backgroundImage: `url(${siriYanguBridge})`}}></section>
         <div className="separator"></div>
-        <section className="portfolio-image-vertical" id="siri-yangu-palm-tree"></section>
+        <section className="portfolio-image-vertical" style={{backgroundImage: `url(${siriYanguPalmTree})`}}></section>
         <div className="separator"></div>
-        <section className="portfolio-image-vertical" id="siri-yangu-boat"></section>
+        <section className="portfolio-image-vertical" style={{backgroundImage: `url(${siriYanguBoat})`}}></section>
         <div className="separator"></div>
-        <section className="portfolio-image-vertical" id="siri-yangu-tree"></section>
+        <section className="portfolio-image-vertical" style={{backgroundImage: `url(${siriYanguTree})`}}></section>
         <div className="separator"></div>
-        <section className="portfolio-image-vertical" id="siri-yangu-balcony"></section>
+        <section className="portfolio-image-vertical" style={{backgroundImage: `url(${siriYanguBalcony})`}}></section>
         <div className="separator"></div>
-        <section className="portfolio-image-vertical" id="west35-pic1"></section>
+        <section className="portfolio-image-vertical" style={{backgroundImage: `url(${west35Pic1})`}}></section>
         <div className="separator"></div>
-        <section className="portfolio-image-vertical" id="west35-pic2"></section>
+        <section className="portfolio-image-vertical" style={{backgroundImage: `url(${west35Pic2})`}}></section>
       </div>
     )
   }
