eb288266422ec06682d5148e2c9da8bbfc031644
ian-ramos <ramos.ianc@gmail.com>
2018-02-20 13:02:57 -0500

    added some pictures & separators, but need to fix width for vertical pics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import ImageContainer from './components/ImageContainer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ImageContainer />
       </div>
     );
   }
--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import '../App.css'
 
 class ImageContainer extends React.Component {
 
   render() {
     return (
-
+      <div>
+        <section className="portfolio-image" id="bamboo"></section>
+        <div className="separator"></div>
+        <section className="portfolio-image" id="irish"></section>
+      </div>
     )
   }
 }
