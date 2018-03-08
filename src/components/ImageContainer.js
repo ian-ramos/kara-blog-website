2df2be937e7ed702c5c9e86137b05c5422bbcd50
ian-ramos <ramos.ianc@gmail.com>
2018-03-07 23:08:10 -0500

    uploaded to heroku

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -17,26 +17,7 @@ class ImageContainer extends React.Component {
     return (
       <div>
         {/*<section className="portfolio-image-vertical" style={{backgroundImage: `url(${imagePaths['bambooTumalogFalls']})`}} onMouseEnter={this.clicked}></section>
-        <div className="separator"></div>
-        <section className="portfolio-image-horizontal" style={{backgroundImage: `url(${imagePaths['irishHungerMemorial']})`}}></section>
-        <div className="separator"></div>
-        <section className="portfolio-image-horizontal" style={{backgroundImage: `url(${imagePaths['lanternFestival']})`}}></section>
-        <div className="separator"></div>
-        <section className="portfolio-image-vertical" style={{backgroundImage: `url(${imagePaths['sanRemigioBeach']})`}}></section>
-        <div className="separator"></div>
-        <section className="portfolio-image-vertical" style={{backgroundImage: `url(${imagePaths['siriYanguBridge']})`}}></section>
-        <div className="separator"></div>
-        <section className="portfolio-image-vertical" style={{backgroundImage: `url(${imagePaths['siriYanguPalmTree']})`}}></section>
-        <div className="separator"></div>
-        <section className="portfolio-image-vertical" style={{backgroundImage: `url(${imagePaths['siriYanguBoat']})`}}></section>
-        <div className="separator"></div>
-        <section className="portfolio-image-vertical" style={{backgroundImage: `url(${imagePaths['siriYanguTree']})`}}></section>
-        <div className="separator"></div>
-        <section className="portfolio-image-vertical" style={{backgroundImage: `url(${imagePaths['siriYanguBalcony']})`}}></section>
-        <div className="separator"></div>
-        <section className="portfolio-image-vertical" style={{backgroundImage: `url(${imagePaths['west35Pic1']})`}}></section>
-        <div className="separator"></div>
-        <section className="portfolio-image-vertical" style={{backgroundImage: `url(${imagePaths['west35Pic2']})`}}></section>*/}
+        <div className="separator"></div>*/}
         {this.portfolioImages()}
       </div>
     )
