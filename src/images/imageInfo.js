3dff3a13f79b2050cdb7e3241a73f49d30170ca8
ian-ramos <ramos.ianc@gmail.com>
2018-03-08 00:15:00 -0500

    added more pics

--- src/images/imageInfo.js
+++ src/images/imageInfo.js
@@ -1,5 +1,6 @@
 import bambooTumalogFalls from '../images/bamboo-tumalog-falls.jpg'
 import irishHungerMemorial from '../images/irish-hunger-memorial.jpg'
+import irishHungerMemorialSillouette from '../images/irish-hunger-memorial-sillouette.jpg'
 import lanternFestival from '../images/lantern-festival.jpg'
 import sanRemigioBeach from '../images/san-remigio-beach.jpg'
 import siriYanguBalcony from '../images/siri-yangu-balcony.jpg'
@@ -9,10 +10,18 @@ import siriYanguPalmTree from '../images/siri-yangu-palm-tree.jpg'
 import siriYanguTree from '../images/siri-yangu-tree.jpg'
 import west35Pic1 from '../images/west35-pic1.jpg'
 import west35Pic2 from '../images/west35-pic2.jpg'
+import charli1 from '../images/charli1.jpg'
+import charli2 from '../images/charli2.jpg'
+import charli3 from '../images/charli3.jpg'
+import manhattanBridge from '../images/manhattan-bridge.jpg'
+import bridgeGazebo from '../images/bridge-gazebo.jpg'
+import dumboBkn from '../images/dumbo-bkn.jpg'
+import sisterGallery from '../images/sister-gallery.jpg'
 
 const imageInfo = [
   {path: bambooTumalogFalls, style: 'portfolio-image-vertical'},
   {path: irishHungerMemorial, style: 'portfolio-image-horizontal'},
+  {path: irishHungerMemorialSillouette, style: 'portfolio-image-horizontal'},
   {path: lanternFestival, style: 'portfolio-image-horizontal'},
   {path: sanRemigioBeach, style: 'portfolio-image-vertical'},
   {path: siriYanguBalcony, style: 'portfolio-image-vertical'},
@@ -21,7 +30,14 @@ const imageInfo = [
   {path: siriYanguPalmTree, style: 'portfolio-image-vertical'},
   {path: siriYanguTree, style: 'portfolio-image-vertical'},
   {path: west35Pic1, style: 'portfolio-image-vertical'},
-  {path: west35Pic2, style: 'portfolio-image-vertical'}
+  {path: west35Pic2, style: 'portfolio-image-vertical'},
+  {path: charli1, style: 'portfolio-image-horizontal'},
+  {path: charli2, style: 'portfolio-image-horizontal'},
+  {path: charli3, style: 'portfolio-image-horizontal'}
+  {path: manhattanBridge, style: 'portfolio-image-vertical'},
+  {path: bridgeGazebo, style: 'portfolio-image-vertical'},
+  {path: dumboBkn, style: 'portfolio-image-vertical'},
+  {path: sisterGallery, style: 'portfolio-image-horizontal'}
 ]
 
 export default imageInfo
